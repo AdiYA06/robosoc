87a91b4d976be63412e77093ce9ac0e17e1d5f98
xDavid673x <leidavid673@gmail.com>
2025-12-18 15:41:15 +0000

    Added turning angle

--- simulator.py
+++ simulator.py
@@ -73,7 +73,7 @@ class Simulator:
         self.ax.set_ylim(mid_y - max_range, mid_y + max_range)
         self.ax.set_zlim(mid_z - max_range, mid_z + max_range)
 
-    def gait_phase(self, legs, swing_legs, p1, p3, S, A, num_of_steps, step):
+    def gait_phase(self, legs, swing_legs, p1, p3, S, A, num_of_steps, step, angle = 0):
         for t in range(0, num_of_steps + 1, step):
             self.ax.cla()
             self.ax.set_xlim(-300, 300)
@@ -102,6 +102,13 @@ class Simulator:
                     0   + sign * R[1][1] * y,
                     z   + sign * R[2][1] * y,
                 ]
+                R = self.tripot.rotation_matrix(-radians(angle))
+                target_rot = [
+                    R[0][0]*target[0] + R[0][1]*target[1] + R[0][2]*target[2],
+                    R[1][0]*target[0] + R[1][1]*target[1] + R[1][2]*target[2],
+                    R[2][0]*target[0] + R[2][1]*target[1] + R[2][2]*target[2],
+                ]
+                target = target_rot
 
                 leg.inverseKinematics(target)
                 self.plot_leg(leg, leg.forwardKinematics())
@@ -109,7 +116,7 @@ class Simulator:
             plt.draw()
             plt.pause(0.001)
 
-    def moving(self, legs, step=1):
+    def moving(self, legs, step=1, angle = 0):
         num_of_steps = 50
         T = 100
         S = -70
@@ -126,14 +133,16 @@ class Simulator:
             self.gait_phase(
                 legs, tripod_A,
                 p1, p3, S, A,
-                num_of_steps, step
+                num_of_steps, step,
+                angle = angle
             )
 
             # Phase B (second half of cycle)
             self.gait_phase(
                 legs, tripod_B,
                 p1, p3, S, A,
-                num_of_steps, step
+                num_of_steps, step,
+                angle = angle
             )
 
 if __name__ == '__main__':
@@ -149,4 +158,4 @@ if __name__ == '__main__':
         leg.set_angles([90, 30, 120])
 
     sim = Simulator(legs)
-    sim.moving(legs)
+    sim.moving(legs, angle=20)
